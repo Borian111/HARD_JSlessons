c852937f099b50afefb2313fdfe47bc83281357a
Boris Tsarfin <legirovanniy@mail.ru>
2021-02-07 02:47:19 +0400

    les04hardUPD1

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-
-
-/*Переменная lang может принимать 2 значения: 'ru' 'en'.
-
-Написать условия при котором в зависимости от значения lang будут выводится дни недели
-на русском или английском языке. Решите задачу
-через if, 
-через switch-case 
-через многомерный массив без ифов и switch.*/
-
+'use strict'
+//часть 1
 let lang='en';
 
 if (lang==='ru') {
@@ -28,22 +20,14 @@ break;
 }
 
 var dayArray = { 
-    'ru': "понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
-    ,'en': "monday, tuesday, wednesday, Thursday, friday, satterday, sunday"
+    'ru': "понедельник, вторник, среда, четверг, пятница, суббота, воскресенье",
+    'en': "monday, tuesday, wednesday, Thursday, friday, satterday, sunday"
      }
 console.log(dayArray[lang]);
 
+//часть 2
+let namePerson='жека';
 
-
-
-
-/*У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести
- в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, 
- с любым другим значением вывести в консоль “студент”.
-    Решить задачу с помощью нескольких тернарных операторов, без использования if или switch*/
-    
-let namePerson='жека'
-
-namePerson==='Артём' ? console.log('Директор') : console.log();
-namePerson==='Максим' ? console.log('преподаватель') : console.log();
-namePerson!=='Максим' || 'Артём' ? console.log('студент') : console.log();
+namePerson==='Артём' ? console.log('Директор') : console.log('');
+namePerson==='Максим' ? console.log('преподаватель') : console.log('');
+namePerson!=='Максим' || 'Артём' ? console.log('студент') : console.log('');
