5be04611391c83f19021578bed2d71ae2c699337
Boris Tsarfin <legirovanniy@mail.ru>
2021-02-07 12:04:00 +0400

    les4upd3

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 'use strict'
-
+let x='   13575fbfgffgbgb4752345243256546464564565464564564565465464645645656    ';
 function sTr(x) {
     if (typeof(x) !== "string") { //проверка на string
         console.log("Это не строка");
-    }else if (typeof(x) === "string")
-         x = x.toString();
-         x = x.trim();//убираем пробелы
-         
-        if(x.length > 30) {
-            x = x.substr(0, 30) + '...';
-          }
+    }else if (typeof(x) === "string"){
+        x = x.trim();//убираем пробелы
+    }     
+    if(x.length > 30) {
+        x = x.substr(0, 30) + '...';
+    }
 console.log(x);
 }
-sTr('    123445335   ');
+sTr(x);
