2f67a4ddbb347b8194b558d314bcf84f9f0c2031
Boris Tsarfin <legirovanniy@mail.ru>
2021-02-16 00:53:07 +0400

    les7upd2

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ let n = d.getDay();
 
 
 for(let i=0; i<7; i++){
-  if (i !==n) {
+  if ((i===n && i===0)||( i===n && i===6)) {
      let newP = document.createElement("p");
+        newP.innerHTML = "<b><i>"+week[i]+"</i></b>";
+        document.body.append(newP);
+ } else if(i===0 || i===6){
+    let newP = document.createElement("p");
         newP.innerHTML = "<i>"+week[i]+"</i>";
         document.body.append(newP);
-  } else {
+  } else if(i===n){
     let newP = document.createElement("p");
         newP.innerHTML = "<b>"+week[i]+"</b>";
         document.body.append(newP);
+  } else if (0<i<6){
+    let newP = document.createElement("p");
+        newP.innerHTML = week[i];
+        document.body.append(newP);
   }
-};
+}
